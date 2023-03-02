190d796894939d1b7a8c4a30bcafe84b4ca58e46
Peter Brenkus <113976414+brenky7@users.noreply.github.com>
2023-03-02 21:00:14 +0100

    extended class, added methods

--- hash1.cpp
+++ hash1.cpp
@@ -3,33 +3,59 @@
 #include <string>
 using namespace std;
 
-struct node{
-    bool used;
-    int key;
-    string value;
-    node* next;
+class Node{
+    public:
+        bool used;
+        int key;
+        string value;
+        Node* next;
+    Node(string one, int two){
+        value = one;
+        key = two;
+        used = true;
+    }
+    Node* addNode(Node* table, string name){
+        int hashIndex = hashit(name);
+        if (table[(hashIndex%5)]==NULL){
+            return new Node(name, hashIndex);
+        }
+        else{
+            Node* temp = table[(hashIndex%5)];
+            while (temp!=NULL){
+                temp = temp->next;
+            }
+            return new Node(name, hashIndex);
+        }
+    }
+    void printTable(Node* table, int size){
+        Node *temp;
+        for (int i = 0; i < size; i++){
+            temp = table[i];
+            while(temp!=NULL){
+                cout << temp->key;
+                temp==temp->next;
+            }
+        }
+        
+    }
 };
 
-int hashit( string input, int size){
-    int output=0;
-    for (int i=0; i< input.length() ;i++){
-        output+= (input[i]);
+int hashit(string name){
+    int Index=0;
+    for (int i=0; i< name.length() ;i++){
+        Index+= (name[i]);
     }
-    return (output);
+    return (Index);
 }
 
 int main(){
     string input;
     int output;
-    node *arr = new node[5];
-    for (int i=0; i<5; i++){
-        cin >> input;
-        output = hashit(input, 5);
-        arr[(output%5)].value = input;
-        arr[(output%5)].key = output;
+    Node *table = new Node[5];
+    for (int i = 0; i < 5; i++){
+        table[i] = NULL;
     }
-    for (int i=0; i<5; i++){
-        cout << arr[i].value << endl;
-    }
+    
+    printTable(table);
     return 0;
 }
